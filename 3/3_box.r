621435e325622cb2dd120662dfc813df05637736
FedNirvanous451 <teodorzur@gmail.com>
2025-03-30 21:17:23 +0300

    to do stats in python

--- 3/3_box.r
+++ 3/3_box.r
@@ -1,6 +1,7 @@
 library(ggplot2)
 library(readr)
-df <- read.csv("music_genre_dataset.csv")
+getwd()
+df <- read.csv("D:/Documents/Learning/3/R/music_genre_dataset.csv")
 p <- ggplot(df, aes(x = BPM,y = RMS.Energy)) +
 geom_boxplot(fill = '#bb205b',color = 'blue') +
 labs(title = 'boxplot')
