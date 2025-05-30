fd05cb53cd80168c7b0c972f02465655635a9747
FedNirvanous451 <teodorzur@gmail.com>
2025-05-30 19:18:29 +0300

    well done kursachhhh

--- 4/4.r
+++ 4/4.r
@@ -3,13 +3,33 @@ ds <- read.csv("D:/Documents/Learning/3/R/ff.csv")
 summary(ds)
 library(modeest)
 
-hiphop_data <- data$bpm[data$genre == "hiphop"]
+hiphop <- ds$`BPM`[ds$`Код.Жанра` == 1]
 rock <- ds$`BPM`[ds$`Код.Жанра` == 0]
 pop <- ds$`BPM`[ds$`Код.Жанра` == 2]
+ds$genre <- factor(ds$Код.Жанра, levels = c(0, 1, 2),
+ labels = c("рок", "хип-хоп", "поп"))
+anova_result <- aov(BPM ~ genre, data = ds)
 t_test_result <- t.test(rock, pop, var.equal = TRUE)
 
 # Вывод результатов
-print(t_test_result)
+print(anova_result)
+print(summary(anova_result))
+
+
+# Преобразуем Инструментальность в категории
+ds$InstrumentalityCategory <- cut(ds$Инструментальность, 
+                                    breaks = c(-0.01, 0.33, 0.66, 1.0), 
+                                    labels = c("Low", "Medium", "High"),
+                                    include.lowest = TRUE)
+
+# Создаем переменную IsRock (1 — рок, 0 — остальные жанры)
+ds$IsRock <- ifelse(ds$Код.Жанра == 0, 1, 0)
+
+# Тест хи-квадрат
+chi_test <- chisq.test(table(ds$InstrumentalityCategory, ds$IsRock))
+print(chi_test)
+
+
 mode_value <- mfv(ds$BPM)
 print(paste("Мода: ", mode_value))
 variance <- var(ds$BPM)
